d3086350478f1ffda3998415ebf02720ae6a9bf9
ZERO9394 <84468270+ZERO9394@users.noreply.github.com>
2025-03-10 13:54:40 -0300

    Update DesafioXadrezNovato.c

---
+++ DesafioXadrez/DesafioXadrezNovato.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // Movimento da Torre (5 casas para a direita)
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+    printf("\n");
+
+    // Movimento do Bispo (5 casas na diagonal para cima e à direita)
+    printf("Movimento do Bispo:\n");
+    int i = 0;
+    while (i < 5) {
+        printf("Cima, Direita\n");
+        i++;
+    }
+    printf("\n");
+
+    // Movimento da Rainha (8 casas para a esquerda)
+    printf("Movimento da Rainha:\n");
+    int j = 0;
+    do {
+        printf("Esquerda\n");
+        j++;
+    } while (j < 8);
+    
+    return 0;
+}
